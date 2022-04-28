de752de9b49c4f839af073119ad7842456fa1e7e
Daniel-LG <elder.dk@gmail.com>
2022-04-29 08:58:39 +0900

    add tqdm multiprocessing

--- README.md
+++ README.md
@@ -10,23 +10,18 @@ The module currently supports the following types of file: `json`.
 
     # to do:
     
-    # make a temporary dictionary (or permanently stored in a excel in the root folder) so that already-translated words don't have to invoke API
-
-    # multiprocessing / progress bar
+    # make a temporary dictionary (or permanently stored in a excel in the root folder) so that already-translated words don't have to invoke API (have to solve the racing condition because of the multithreading)
 
     # try converting txts into docxs, translate, and turn them back into txt (for speed)
 
     # test (make sure input json # and output json # are the same)write unit test but also write in-functionv validation
 
-
-    # make a report at the end (look into logger), display 1) times API called 2) number of charts sent 3) number of new words added to the glossary excel
-
     # make argparse (be able to set file paths and output paths and excel glossary path)
 
     # make a GUI
 
-    # if a file stops in middle due to error, remember the last position and start over from there next time
+    # if a file stops in middle due to error, remember the last position and start over from there next time (try / except: save a partial file. When starting again, compare the lengths of source file and found target file and if lengths are different, create a new list from the source of the dicts that dont exist in the target and translated them. Then load the existing dicts_list, add the new dicts, and sort, then overwrite the existing target file)
 
-    # create a debug=true mode so that api calls aren't made during development when not necessary
+    # create a debug=true mode so that api calls aren't made during development when not necessary (create and use a mock data with really few chars. regressive discovery is already working one folder is enough for mock purpose)
 
     # when sending a list of strings to google API, change the number of strings so that the combined characters do not exceed 5000 and as many as possible strings can be sent to API at one time OR test converting the json file into docx so that whole file can be sent at once (compare this with the previously completed file and see if translations are identical or close enough) 
--- src/translators/json_files.py
+++ src/translators/json_files.py
@@ -2,6 +2,8 @@ import json
 from pathlib import Path
 from typing import Dict, List
 import logging
+import concurrent.futures
+from tqdm import tqdm
 
 from . import api_handler as ah
 from utils.helpers import create_file_folder
@@ -22,6 +24,35 @@ def get_dict_list(filepath: str) -> List[Dict]:
         return json.load(fp)
 
 
+def get_translated_dict(d):
+
+    bool_map = [isinstance(v, str) for v in d.values()]
+    original_value_list = [v for v in d.values()]
+    string_value_list = [v for v in d.values() if isinstance(v, str)]
+
+    # call Google cloud translation API
+
+    result = ah.translate_text_with_model("en", string_value_list)
+    result = [v["translatedText"] for v in result]
+
+    # create a new list with the translation based on the bool map
+
+    new_list = []
+    for i in range(0, len(original_value_list)):
+        if bool_map[i] is True:
+            new_list.append(result[i])
+        else:
+            new_list.append(original_value_list[i])
+
+    return dict(zip(d.keys(), new_list))
+
+
+def tqdm_multithreading(f, dict_list):
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = list(tqdm(executor.map(f, dict_list), total=len(dict_list)))
+    return results
+
+
 def get_translated_dict_list(dict_list: List[Dict]) -> List[Dict]:
     """Handle translation.
 
@@ -40,38 +71,22 @@ def get_translated_dict_list(dict_list: List[Dict]) -> List[Dict]:
     ------
     List[dict]
     """
-    new_dict_list = []
-    api_calls = 0
-    api_chars = 0
 
-    for d in dict_list:
+    new_dict_list = tqdm_multithreading(get_translated_dict, dict_list)
 
-        bool_map = [isinstance(v, str) for v in d.values()]
-        original_value_list = [v for v in d.values()]
-        string_value_list = [v for v in d.values() if isinstance(v, str)]
+    meta = {
+        "api_calls": len(dict_list),
+        "api_chars": len(
+            "".join([v for d in dict_list for k, v in d.items() if isinstance(v, str)])
+        ),
+    }
 
-        # google cloud translation API call made here
-        # count api calls and number of chars sent
-
-        api_calls += 1
-        api_chars += len(''.join(string_value_list))
-        result = ah.translate_text_with_model("en", string_value_list)
-        result = [v["translatedText"] for v in result]
-
-        new_list = []
-        for i in range(0, len(original_value_list)):
-            if bool_map[i] is True:
-                new_list.append(result[i])
-            else:
-                new_list.append(original_value_list[i])
-
-        new_dict = dict(zip(d.keys(), new_list))
-        new_dict_list.append(new_dict)
-
-    return new_dict_list, {"api_calls": api_calls, "api_chars": api_chars}
+    return new_dict_list, meta
 
 
-def save_to_new_file(filepath: Path, ff: str, dict_list: List[Dict], output_path: str) -> None:
+def save_to_new_file(
+    filepath: Path, ff: str, dict_list: List[Dict], output_path: str
+) -> None:
     """Save the new file.
 
     If the output folder doesn't exist, a new output folder will be created
